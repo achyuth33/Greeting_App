53b56dbbd656b138d874d5ebf616a1111cd2b6dd
Achyuth <achyuth0910@gmail.com>
2022-08-04 07:13:32 +0000

    Greeting app

---
+++ src/components/GreetUser/index.js
@@ -0,0 +1,30 @@
+import {useState} from 'react'
+
+import {
+  MainContainer,
+  NameInput,
+  MsgContent,
+  NameText,
+} from './styledComponents'
+
+const GreetUser = () => {
+  const [name, setName] = useState('')
+
+  const onChangeName = event => setName(event.target.value)
+
+  return (
+    <MainContainer>
+      <NameInput
+        type="text"
+        placeholder="Your name"
+        value={name}
+        onChange={onChangeName}
+      />
+      <MsgContent>
+        Hello <NameText>{name}</NameText>
+      </MsgContent>
+    </MainContainer>
+  )
+}
+
+export default GreetUser
